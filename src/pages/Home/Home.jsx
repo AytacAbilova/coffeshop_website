de8bbdf9ac951fa4eac099940301d42ea952605b
Beyim55 <bayimer-ap217@code.edu.az>
2026-05-13 11:39:24 +0400

    hero responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../../components/Hero/Hero";
+import Reservation from "../Reservation/Reservation";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Reservation/>
     </div>
   );
 };
